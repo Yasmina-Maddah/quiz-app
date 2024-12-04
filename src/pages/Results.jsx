db0d74833df95a2869457cada819ca4eaa96d1f6
Yasmina Maddah <maddahyasmina@gmail.com>
2024-12-04 22:57:31 +0200

    Added the Results page

---
+++ src/pages/Results.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Results = () => {
+  return (
+    <div>
+      <h2>Your Results</h2>
+      <p>Your total score: [Implement Score Persistence]</p>
+    </div>
+  );
+};
+
+export default Results;
