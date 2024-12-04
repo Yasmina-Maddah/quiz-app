942fab258d2e5ea3e026279aef3634aab8eab96f
Yasmina Maddah <maddahyasmina@gmail.com>
2024-12-04 22:54:28 +0200

    Edited the App.js file

---
+++ src/utils/quizzesData.jsx
@@ -0,0 +1,33 @@
+export const quizzes = [
+    {
+      id: 1,
+      title: "General Knowledge",
+      questions: [
+        {
+          id: 1,
+          text: "What is the capital of France?",
+          options: ["Paris", "London", "Rome", "Berlin"],
+          answer: "Paris",
+        },
+        {
+          id: 2,
+          text: "What is 5 + 5?",
+          options: null, 
+          answer: "10",
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "Science Quiz",
+      questions: [
+        {
+          id: 1,
+          text: "What planet is known as the Red Planet?",
+          options: ["Earth", "Mars", "Jupiter", "Venus"],
+          answer: "Mars",
+        },
+      ],
+    },
+  ];
+  
